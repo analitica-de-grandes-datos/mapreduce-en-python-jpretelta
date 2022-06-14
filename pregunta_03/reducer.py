5fd17be9903e355bf99e387f3742078fed4dedba
jpretelta <jjpretelta@unal.edu.co>
2022-06-14 09:02:38 -0500

    Resp Pregunta 03

--- pregunta_03/reducer.py
+++ pregunta_03/reducer.py
@@ -1,3 +1,22 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+import operator
+
+if __name__ == '__main__':
+
+    records = list()
+
+    for line in sys.stdin:
+        key, val = line.split("\t")
+        val = int(val)
+
+        records.append((key, val))
+
+    #ordernar lista de tuplas
+    records = sorted(records, key=operator.itemgetter(1), reverse=False)
+
+    #imprimir el diccionario
+    for key, val in records:
+        sys.stdout.write("{},{}\n".format(key, val))
