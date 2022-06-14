4b31f8b1d6305ede66c146e2e8286956aa88c259
jpretelta <jjpretelta@unal.edu.co>
2022-06-13 22:07:11 -0500

    Resp Pregunta 01

--- pregunta_01/mapper.py
+++ pregunta_01/mapper.py
@@ -1,3 +1,12 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+
+    for line in sys.stdin:
+        fields = line.strip().split(',',5)
+        credit_history =  fields[2]
+
+        sys.stdout.write("{}\t1\n".format(credit_history))
