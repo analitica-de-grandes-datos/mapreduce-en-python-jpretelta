afbbf033cfdd7bc744d04268f848fc8a5a266997
jpretelta <jjpretelta@unal.edu.co>
2022-06-14 12:01:20 -0500

    Resp Pregunta 05

--- pregunta_05/mapper.py
+++ pregunta_05/mapper.py
@@ -1,3 +1,14 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+
+    for line in sys.stdin:
+        fields = line.strip().split('   ')
+        date = fields[1]
+        splitted_date = date.split('-')
+        month=splitted_date[1]
+
+        sys.stdout.write("{}\t1\n".format(month))
