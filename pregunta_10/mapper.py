0abb296b72659bd3b3f7ee472523700c3ca7b8da
jpretelta <jjpretelta@unal.edu.co>
2022-06-14 18:25:43 -0500

    Resp Pregunta 10

--- pregunta_10/mapper.py
+++ pregunta_10/mapper.py
@@ -1,3 +1,13 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+
+    for line in sys.stdin:
+        fields = line.strip().split('\t')
+        number=fields[0]
+        letters=fields[1]
+       
+        sys.stdout.write("{}\t{}\n".format(number,letters))
--- pregunta_10/reducer.py
+++ pregunta_10/reducer.py
@@ -1,3 +1,28 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+if __name__ == '__main__':
+
+    dict_letters = dict()
+
+    for line in sys.stdin:
+        
+        number, letters = line.split("\t")
+        letters = letters.strip().split(",")
+
+        for i in letters:
+            if i not in dict_letters.keys():
+                dict_letters[i] = number
+            else:
+                dict_letters[i] = dict_letters[i] + ',' + number
+         
+        tuple_letters = sorted(dict_letters.items(), key=lambda x: x[0])
+
+    for letter, associated_numbers in tuple_letters:        
+        unordered_numbers = associated_numbers.split(",") 
+        ordered_numbers = sorted(unordered_numbers, key=int)
+        ordered_numbers = ",".join(ordered_numbers)
+
+        sys.stdout.write("{}\t{}\n".format(letter, ordered_numbers))
