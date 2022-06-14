f0cbc9acdeb4d57725231a3a507a71c5ec58bd65
jpretelta <jjpretelta@unal.edu.co>
2022-06-14 14:34:30 -0500

    Resp Pregunta 07

--- pregunta_07/mapper.py
+++ pregunta_07/mapper.py
@@ -1,3 +1,14 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+import sys
+
+if __name__ == "__main__":
+
+    for line in sys.stdin:
+        fields = line.strip().split('   ')
+        letter=fields[0]
+        date=fields[1]
+        value=fields[2]
+        
+        sys.stdout.write("{}\t{}\t{}\n".format(letter,date,value))
--- pregunta_07/reducer.py
+++ pregunta_07/reducer.py
@@ -1,3 +1,19 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+
+if __name__ == '__main__':
+
+    tuple_list = []
+    
+    for line in sys.stdin:
+        key, date, value = line.split("\t")
+        value = int(value)
+        tuple = (key, date, value)
+        tuple_list.append(tuple)
+        
+    tuple_list = sorted(tuple_list, key=lambda x: (x[0],x[2]))
+
+    for tuple in tuple_list:
+        sys.stdout.write("{}   {}   {}\n".format(tuple[0], tuple[1], tuple[2]))
