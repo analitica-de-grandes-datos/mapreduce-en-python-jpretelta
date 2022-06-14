d7e701c3eb687d8cbb99dc82e472905284d06e46
jpretelta <jjpretelta@unal.edu.co>
2022-06-14 14:59:02 -0500

    Resp Pregunta 08

--- pregunta_08/reducer.py
+++ pregunta_08/reducer.py
@@ -1,3 +1,32 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+from statistics import mean
+import sys
+
+if __name__ == '__main__':
+
+    curkey = None
+    tuple_list = []
+
+    for line in sys.stdin:
+
+        key, val = line.split("\t")
+        val = float(val)
+        
+        if key == curkey:
+            tuple_list.append(val)
+        else:
+            if curkey is not None:
+                suma = sum(tuple_list)
+                avg = mean(tuple_list)
+                tuple_list.clear()                
+
+                sys.stdout.write("{}\t{}\t{}\n".format(curkey, suma, avg))
+
+            curkey = key
+            tuple_list.append(val)               
+
+    suma = sum(tuple_list)
+    avg = mean(tuple_list)
+    sys.stdout.write("{}\t{}\t{}\n".format(curkey, suma, avg))
